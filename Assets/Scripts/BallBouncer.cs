3848f513f67a786f515569249a934415d7e5f333
JeanineBray <6375151+JeanineBray@users.noreply.github.com>
2018-08-03 09:57:42 -0400

    Add files via upload

---
+++ Assets/Scripts/BallBouncer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBouncer : MonoBehaviour
+{
+    private Vector3 initialVelocity;
+    private float minVelocity = 10f;
+    private Vector3 lastFrameVelocity;
+    private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.velocity = initialVelocity;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        lastFrameVelocity = rb2d.velocity;
+	}
+
+    void Bounce(Vector3 collisionNormal)
+    {
+        var speed = lastFrameVelocity.magnitude;
+        var direction = Vector3.Reflect(lastFrameVelocity.normalized, collisionNormal);
+
+        Debug.Log("Out Direction: " + direction);
+        rb2d.velocity = direction * Mathf.Max(speed, minVelocity);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Bounce(collision.contacts[0].normal);
+    }
+}
---
+++ Assets/Scripts/BounceTowardPlayer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceTowardPlayer : MonoBehaviour
+{
+    private float bias = 0.5f;
+    private Vector3 initialVelocity;
+    private Transform playerTransform;
+    private float bounceVelocity = 10f;
+    private Vector3 lastFrameVelocity;
+    private Rigidbody2D rb2d;
+
+    private void OnEnable()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.velocity = initialVelocity;
+    }
+
+    private void Update()
+    {
+        lastFrameVelocity = rb2d.velocity;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Bounce(collision.contacts[0].normal);
+    }
+
+    private void Bounce(Vector3 collisionNormal)
+    {
+        var speed = lastFrameVelocity.magnitude;
+        var bounceDirection = Vector3.Reflect(lastFrameVelocity.normalized, collisionNormal);
+        var directionToPlayer = playerTransform.position - transform.position;
+
+        var direction = Vector3.Lerp(bounceDirection, directionToPlayer, bias);
+
+        Debug.Log("Out Direction: " + direction);
+        rb2d.velocity = direction * bounceVelocity;
+    }
+}
